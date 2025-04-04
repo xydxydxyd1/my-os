741c4be734d3e744eb6f26535a65f025ee813945
Eric Xu <xydxydxyd1@gmail.com>
2025-04-03 22:20:53 -0700

    add: objdump and qemu

--- arm-linux.sh
+++ arm-linux.sh
@@ -1,4 +1,7 @@
 # Aliases for ARM linux
 gccpath="$(pwd)/utils/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu"
+export GNUPATH="$(pwd)/utils/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu"
 alias gcc="$gccpath-gcc"
 alias as="$gccpath-as"
+alias objdump="$gccpath-objdump"
+alias qemu="qemu-aarch64 -L $GNUPATH/aarch64-none-linux-gnu/libc/"
