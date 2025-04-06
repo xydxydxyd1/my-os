4bdcb00838588b1f7a4fb924dc67d807e0f74f79
Eric Xu <xydxydxyd1@gmail.com>
2025-04-05 18:03:34 -0700

    add: prefix

--- arm-linux.sh
+++ arm-linux.sh
@@ -1,8 +1,8 @@
 # Aliases for ARM linux
-gccpath="$(pwd)/utils/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu"
+export PREFIX="$(pwd)/utils/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu"
 export GNUPATH="$(pwd)/utils/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu"
-alias gcc="$gccpath-gcc"
-alias as="$gccpath-as"
-alias objdump="$gccpath-objdump"
-alias ld="$gccpath-ld"
+alias gcc="$PREFIX-gcc"
+alias as="$PREFIX-as"
+alias objdump="$PREFIX-objdump"
+alias ld="$PREFIX-ld"
 alias qemu="qemu-aarch64 -L $GNUPATH/aarch64-none-linux-gnu/libc/"
