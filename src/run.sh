2905155bdd84e37e6d19a780263fbe2aba736fbc
Eric Xu <xydxydxyd1@gmail.com>
2025-04-06 11:27:45 -0700

    add: draft aarch64 version of guide 2

---
+++ src/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# run.sh 
+#
+while getopts "" opt; do
+    case $opt in
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    ;;
+    esac
+done
+if [[ "$#" -ne 0 ]]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
+
+qemu-system-aarch64 -s -S -M raspi4b -nographic -kernel test.bin
