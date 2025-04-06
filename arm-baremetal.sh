e4553b167f32a1555e56b2cc895cd7e402de1da2
Eric Xu <xydxydxyd1@gmail.com>
2025-04-05 18:01:50 -0700

    add: prefix

--- arm-baremetal.sh
+++ arm-baremetal.sh
@@ -1,3 +1,3 @@
 # Aliases for baremetal
-gccpath="$(pwd)/utils/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf"
+export PREFIX="$(pwd)/utils/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf"
 alias gcc="$gccpath-gcc"
