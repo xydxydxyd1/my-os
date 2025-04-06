a49455988b1ff656c0756009e819e542a2bf630f
Eric Xu <xydxydxyd1@gmail.com>
2025-04-06 10:26:35 -0700

    add: test from https://balau82.wordpress.com/2010/02/28/hello-world-for-bare-metal-arm-using-qemu/

---
+++ test/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# run.sh 
+#
+while getopts "" opt; do
+    case $opt in
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    ;;
+    esac
+done
+if [[ "$#" -ne 0 ]]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
+
+qemu-system-arm -M versatilepb -m 128M -nographic -kernel test.bin
