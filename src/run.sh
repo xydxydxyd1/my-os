c0de034a3db74ba1a30442383b511a4b5932c7d0
Eric Xu <xydxydxyd1@gmail.com>
2025-04-09 12:43:43 -0700

    add: run script for src

--- src/run.sh
+++ src/run.sh
@@ -2,9 +2,18 @@
 # run.sh 
 #
 
-qemu-system-aarch64 \
+qemu_cmd="qemu-system-aarch64 \
+    -s -S \
     -M versatilepb \
     -cpu cortex-a72 \
     -monitor stdio \
     -kernel ./test.elf \
-    $*
+    $*"
+
+make
+
+tmux split-window -h
+tmux last-pane
+tmux send-keys -t - "$qemu_cmd" Enter
+sleep 3
+aarch64-none-elf-gdb -ex "target remote :1234" test.elf
