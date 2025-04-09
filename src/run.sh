ed4610359dea236c9ee4449eb0c0c2c54305884f
Eric Xu <xydxydxyd1@gmail.com>
2025-04-08 17:36:23 -0700

    add: pass on additional arguments to qemu

--- src/run.sh
+++ src/run.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 # run.sh 
 #
-while getopts "" opt; do
-    case $opt in
-	\?)
-	    echo "Invalid option: -$OPTARG" >&2
-	    ;;
-    esac
-done
-if [[ "$#" -ne 0 ]]; then
-    echo "Illegal number of parameters" >&2
-    exit 1
-fi
 
 qemu-system-aarch64 \
     -M versatilepb \
     -cpu cortex-a72 \
     -monitor stdio \
-    -device loader,file=./test.bin
+    -device loader,file=./test.bin \
+    $*
