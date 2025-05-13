0e6c55d227fee4a7ed9f06f767316f9b1f28fc67
Eric Xu <xydxydxyd1@gmail.com>
2025-05-13 10:57:27 -0700

    uart: add initialization

--- rpi/peripherals/uart.h
+++ rpi/peripherals/uart.h
@@ -19,7 +19,15 @@
 // UART Interface utils
 
 typedef struct {
-    uint32_t _DR;
+    uint32_t _DR; // Data
+    uint32_t _RSRECR; // Receive status/Error clear
+    uint8_t _UNUSED0[24];
+    uint32_t _FR; // Flag
+    uint8_t _UNUSED1[4];
+    uint32_t _IBRD; // Integer baud rate divisor
+    uint32_t _FBRD; // Fractional baud rate divisor
+    uint32_t _LCRH; // Line control
+    uint32_t _CR; // Control
 } UART_Regs;
 
 #define UART0 ((volatile UART_Regs * const)0xfe201000)
@@ -28,7 +36,15 @@ typedef struct {
 #define UART4 ((volatile UART_Regs * const)0xfe201800)
 #define UART5 ((volatile UART_Regs * const)0xfe201a00)
 
+#define UART_BAUDRATE 9600
+
+
 // UART interface
+
+/**
+ * Initialize a UART device with default configuration
+ */
+void UART_init(volatile UART_Regs* uart);
 
 /**
  * Transmit a character with UART
@@ -37,7 +53,7 @@ typedef struct {
  * uart - the UART device with which to transmit `c`.
  * c - the character to transmit
  */
-void UART_putchar(volatile UART_Regs* const uart, char c);
+void UART_putchar(volatile UART_Regs* uart, char c);
 
 /**
  * Transmit a string
@@ -46,7 +62,7 @@ void UART_putchar(volatile UART_Regs* const uart, char c);
  * uart - the UART device with which to transmit `str`
  * str - the null-terminated string to transmit
  */
-void UART_print(volatile UART_Regs* const uart, char* str);
+void UART_print(volatile UART_Regs* uart, char* str);
 
 
 #endif /* ifndef UART_H */
